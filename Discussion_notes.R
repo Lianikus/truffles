d1b0181f8c40bfc10b7609349ca0db128c833c96
Lianikus <liabaumann02@gmail.com>
2024-05-29 18:25:24 +0200

    Persistence mit clone corrected MLG dataset

---
+++ Discussion_notes.R
@@ -0,0 +1,18 @@
+Points for discussion
+
+Non-systematic and nondestructive
+harvests are therefore likely dominated by specific aroma
+concentrations, fruitbody dimensions and locations, as well as
+hunter/dog ability. The aroma profile of truffles seems to be influenced
+by individual genotypes @molinier2015, and not by the geographical
+origin or maturity level @niimi2021. Since the development of aromas
+most likely depends on the presence of specific bacteria
+@splivallo2016a, no correlation between gleba colour and aroma intensity
+has been reported. Depending on the percentage of fully pigmented
+spores, and thus the level of maturity and ripening @molinier2016, the
+gleba of Burgundy truffles can vary from white-ochre to dark brown.
+Differences in the colour and texture of fruitbodies have been proofed
+useful visual criteria for species identification and distinction
+@chatin1887, which may otherwise be considered genetically identical,
+such as Tuber. aestivum and Tuber uncinatum @molinier2013.
+(@buntgenEcoarchaeologicalExcavationTechniques2021).
